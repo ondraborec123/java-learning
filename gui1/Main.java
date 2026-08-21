7248d8a300db5818783b6d4e00970b9264be1881
ondraborec123 <selucky.o@ssmg.cz>
2026-08-21 18:46:10 +0200

    html+css in java

--- gui1/Main.java
+++ gui1/Main.java
@@ -1,24 +1,31 @@
-/*
-Still under construction this is not done
-*/
-
-
+import java.awt.BorderLayout;
 import java.awt.Color;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
 public class Main {
     public static void main(String[] args) {
-
         ImageIcon image = new ImageIcon("computer_off.png");
-
         JLabel label = new JLabel("hello world");
         label.setIcon(image);
         label.setHorizontalTextPosition(JLabel.CENTER); label.setVerticalTextPosition(JLabel.BOTTOM);
-        label.setForeground(new Color(0,0,255));
-        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setHorizontalAlignment(JLabel.CENTER); label.setVerticalAlignment(JLabel.CENTER);
+        label.setForeground(Color.RED);
+
+        JLabel text = new JLabel("<html><div style='width:200px;'>"+"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."+"</div></html>");
+        text.setHorizontalAlignment(JLabel.LEFT); text.setVerticalAlignment(JLabel.TOP);
+
+        Panel leftPanel = new Panel(); leftPanel.setBounds(0,0,300,600);
+        leftPanel.setLayout(new BorderLayout());
+        leftPanel.setBackground(Color.YELLOW);
+        Panel rightPanel = new Panel(); rightPanel.setBounds(300,0,300,600);
+        rightPanel.setLayout(new BorderLayout());
+        rightPanel.setBackground(Color.GREEN);
 
         Window window = new Window();
-        window.add(label);
+        window.add(leftPanel);
+        leftPanel.add(label);
+        rightPanel.add(text);
+        window.add(rightPanel);
     }
 }
--- gui1/Window.java
+++ gui1/Window.java
@@ -1,13 +1,12 @@
 import javax.swing.JFrame;
-import java.awt.Color;
 
 public class Window extends JFrame {
     Window() {
         this.setSize(600,600);
-        this.getContentPane().setBackground(new Color(0,255,0));
         this.setTitle("java je cool");
         this.setResizable(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
         this.setVisible(true);
     }
 }
